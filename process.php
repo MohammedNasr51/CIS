a6264b9a7e62513034dbb67750abf02cdbe0f298
abc <mnasr0868@gmail.com>
2023-10-03 03:30:02 +0300

    using Post request

--- process.php
+++ process.php
@@ -1,5 +1,11 @@
 <?php
+echo nl2br('processing ...<br>');
+// Get variables
+if (isset($_GET['name'])) {
+    echo 'GET : your name is ' . $_GET['name'];
+}
 
-if (isset($_GET['name'])){
-    echo 'your name is ' . $_GET['name'];
+// POST variables
+if (isset($_POST['name'])) {
+    echo 'POST : your name is ' . $_POST['name'];
 }
